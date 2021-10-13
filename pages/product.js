7796ecef715abf23a7cc5b8cf836a49e3d151f04
yutthapichai chaiwong <yutthapichai.c@skanhubrewards.com>
2021-10-13 15:58:36 +0700

    update product

--- pages/product.js
+++ pages/product.js
@@ -164,7 +164,7 @@ export default function product() {
                   <img
                     src={i.p_image}
                     className="card-img-top"
-                    key={i._id}
+                    key={k}
                     alt="..."
                   />
                   <div className="card-body">
