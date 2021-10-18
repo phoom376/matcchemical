d43f30f6139bfd00aef99fce8a5f2b56a6d8d777
yutthapichai chaiwong <yutthapichai.c@skanhubrewards.com>
2021-10-18 16:20:54 +0700

    update time

--- pages/board.js
+++ pages/board.js
@@ -59,12 +59,12 @@ export default function Board() {
                       <h3>Board Name: {i.b_name}</h3>
                       <p>
                         BOARD{" "}
-                        {tmpDT.getFullYear() !== now.getFullYear() ||
-                        tmpDT.getDate() !== now.getDate() ||
-                        tmpDT.getDay() !== now.getDay() || tmpDT.getHours() !== tmpDT.getHours() || tmpDT.getMinutes() !== now.getMinutes() || tmpDT.getSeconds() + 30 < now.getSeconds() ? (
-                          <span style={{color:"red"}}> OFFLINE</span>
+                        {tmpDT.getFullYear() === now.getFullYear() ||
+                        tmpDT.getDate() === now.getDate() ||
+                        tmpDT.getDay() === now.getDay() || tmpDT.getHours() === tmpDT.getHours() || tmpDT.getMinutes() === now.getMinutes() || tmpDT.getSeconds() + 30 > now.getSeconds() ? (
+                          <span style={{color:"red"}}>ONLINE</span>
                         ) : (
-                          <span style={{color:"green"}}>ONLINE</span>
+                          <span style={{color:"green"}}>OFFLINE</span>
                         )}
                       </p>
 
