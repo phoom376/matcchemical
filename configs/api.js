69091a928a83f30cbeeefd2502d1af6a8f529695
yutthapichai chaiwong <yutthapichai.c@skanhubrewards.com>
2021-10-20 10:01:59 +0700

    update switch

---
+++ configs/api.js
@@ -0,0 +1,26 @@
+const test = "";
+const server = "";
+
+
+const API = {
+    BOARD:`${base}/line`,
+    LOGIN: `${base}/auth/login`,
+    AUTH: `${base}/auth`,
+    REFRESH_TOKEN: `${base}/auth/refresh-token`,
+    ORDER: `${base}/orders`,
+    PRODUCT: `${base}/product`,
+    CATEGORY: `${base}/category`,
+    RECEIPT: `${base}/receipt`,
+    PROMOTION: `${base}/promotion`,
+    UPLOAD: `${base}/upload`,
+    IMAGE: `${base}/image`, 
+    REWARD: `${base}/reward`,
+    REDEMPTION: `${base}/redemption`,
+    COUPON: `${base}/coupon`,
+    SHIPPING: `${base}/shipping`,
+    SURVEY: `${base}/survey`,
+    PICKUP:`${base}/pickup`
+  };
+  
+  export default API;
+  
--- pages/board.js
+++ pages/board.js
@@ -3,9 +3,15 @@ import { useEffect, useState } from "react";
 import { ProgressBar } from "react-bootstrap";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+const Swal = require("sweetalert2");
+
 export default function Board() {
   const [boards, setBoards] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const server = "https://boardapi.herokuapp.com";
+  const test = "http://localhost:4002";
+
   useEffect(() => {
     const gProduct = setInterval(() => {
       getBoard();
@@ -17,16 +23,44 @@ export default function Board() {
   }, []);
 
   const getBoard = () => {
-    axios.get("https://boardapi.herokuapp.com/board").then((res) => {
+    axios.get(`${server}/boards`).then((res) => {
       setBoards(res.data);
       setLoading(false);
     });
   };
 
+  const upDateBoard = (id, scc) => {
+    axios
+      .post(`${server}/updateControl`, { b_id: id, scc: scc })
+      .then((res) => {
+        let timerInterval;
+        Swal.fire({
+          title: "PLEASE WAIT!",
+          timer: 2000,
+          timerProgressBar: true,
+          didOpen: () => {
+            Swal.showLoading();
+
+            timerInterval = setInterval(() => {
+              
+            }, 100);
+          },
+          willClose: () => {
+            clearInterval(timerInterval);
+          },
+        }).then((result) => {
+          /* Read more about handling dismissals below */
+          if (result.dismiss === Swal.DismissReason.timer) {
+            console.log("I was closed by the timer");
+          }
+        });
+      });
+  };
+
   if (loading) {
     return (
       <div>
-        <h1>Loading...</h1>
+        <p>Loading...</p>
       </div>
     );
   } else {
@@ -41,30 +75,24 @@ export default function Board() {
               const newDateTime = dateTime.split("T");
               const date = newDateTime[0];
               const time = newDateTime[1].split(".");
-
-              const newformat = date + " " + time[0];
-              const dayjs = require("dayjs");
               const now = new Date();
-              const tmpDT = new Date(newformat);
-              console.log(tmpDT.getSeconds());
-              console.log(now.getSeconds());
-              {
-                /* console.log(newformat);
-              console.log(now.format("YYYY-MM-DD hh:mm:ss")); */
-              }
+              const tmpDT = new Date(dateTime);
+
               return (
                 <div className="board" key={i._id}>
                   <div className="mb-3 ">
                     <div className="board-header">
                       <h3>Board Name: {i.b_name}</h3>
                       <p>
-                        BOARD{" "}
-                        {tmpDT.getFullYear() === now.getFullYear() &&
-                        tmpDT.getDate() === now.getDate() &&
-                        tmpDT.getDay() === now.getDay() && tmpDT.getHours() === tmpDT.getHours() && tmpDT.getMinutes() === now.getMinutes() ? (
-                          <span style={{color:"green"}}>ONLINE</span>
+                        BOARD STATUS:{" "}
+                        {tmpDT.getFullYear() !== now.getFullYear() ||
+                        tmpDT.getDate() !== now.getDate() ||
+                        tmpDT.getDay() !== now.getDay() ||
+                        tmpDT.getHours() !== tmpDT.getHours() ||
+                        tmpDT.getMinutes() !== now.getMinutes() ? (
+                          <span style={{ color: "red" }}>OFFLINE</span>
                         ) : (
-                          <span style={{color:"red"}}>OFFLINE</span>
+                          <span style={{ color: "green" }}>ONLINE</span>
                         )}
                       </p>
 
@@ -74,6 +102,39 @@ export default function Board() {
                           {date} {time[0]}
                         </span>
                       </p>
+                    </div>
+                    <div className="button-box">
+                      <div className="box">
+                        <p className="button-title">SCC</p>
+                        {i.scc === 0 ? (
+                          <button
+                            className="btn btn-outline-warning"
+                            onClick={() => upDateBoard(i._id, 1)}
+                          >
+                            OFF
+                          </button>
+                        ) : (
+                          <button
+                            className="btn btn-outline-success"
+                            onClick={() => upDateBoard(i._id, 0)}
+                          >
+                            ON
+                          </button>
+                        )}
+                      </div>
+                      <div className="box">
+                        <p className="button-title">PH</p>
+                        <form>
+                          <div className="mb-3">
+                            <label>START:</label>
+                            <input type="number" min="0" max="14"></input>
+                          </div>
+                        </form>
+                        <div className="mb-3">
+                          <label>STOP:</label>
+                          <input type="number" min="0" max="14"></input>
+                        </div>
+                      </div>
                     </div>
                     <div className="board-box">
                       <div>
