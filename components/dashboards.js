4955e07783014b3d65278813ab9b55ccdb161326
yutthapichai chaiwong <yutthapichai.c@skanhubrewards.com>
2022-02-15 16:38:50 +0700

    update

--- components/dashboards.js
+++ components/dashboards.js
@@ -100,115 +100,117 @@ const Dashboards = ({
             </thead>
             <tbody>
               {boards.map((i, k) => {
-                const dateTime = i.uDate;
-                const now = Date(Date.now);
-                const tmpDT = dateTime.split(" ");
-                const tmpNow = now.split(" ");
-                const nTime = tmpNow[4].toString();
-                const nDay = tmpNow[2].toString();
-                const nYear = tmpNow[3].toString();
-                const nTmpTime = nTime.split(":");
-                const nHour = nTmpTime[0];
-                const nMin = nTmpTime[1];
-                const nSec = nTmpTime[2];
-                const time = tmpDT[4].toString();
-                const day = tmpDT[2].toString();
-                const year = tmpDT[3].toString();
-                const tmpTime = time.split(":");
-                const Hour = tmpTime[0];
-                const Min = tmpTime[1];
-                const Sec = tmpTime[2];
-                const Months = [
-                  "Jan",
-                  "Feb",
-                  "Mar",
-                  "Apr",
-                  "May",
-                  "Jun",
-                  "Jul",
-                  "Aug",
-                  "Sep",
-                  "Oct",
-                  "Nov",
-                  "Dec",
-                ];
-                let tmpMonth = 0;
-                let nTmpMonth = 0;
-                for (let i = 0; i < Months.length; i++) {
-                  if (tmpDT[1] == Months[i]) {
-                    tmpMonth = i + 1;
+                if (i.ph !== null || i.ec !== null) {
+                  const dateTime = i.uDate;
+                  const now = Date(Date.now);
+                  const tmpDT = dateTime.split(" ");
+                  const tmpNow = now.split(" ");
+                  const nTime = tmpNow[4].toString();
+                  const nDay = tmpNow[2].toString();
+                  const nYear = tmpNow[3].toString();
+                  const nTmpTime = nTime.split(":");
+                  const nHour = nTmpTime[0];
+                  const nMin = nTmpTime[1];
+                  const nSec = nTmpTime[2];
+                  const time = tmpDT[4].toString();
+                  const day = tmpDT[2].toString();
+                  const year = tmpDT[3].toString();
+                  const tmpTime = time.split(":");
+                  const Hour = tmpTime[0];
+                  const Min = tmpTime[1];
+                  const Sec = tmpTime[2];
+                  const Months = [
+                    "Jan",
+                    "Feb",
+                    "Mar",
+                    "Apr",
+                    "May",
+                    "Jun",
+                    "Jul",
+                    "Aug",
+                    "Sep",
+                    "Oct",
+                    "Nov",
+                    "Dec",
+                  ];
+                  let tmpMonth = 0;
+                  let nTmpMonth = 0;
+                  for (let i = 0; i < Months.length; i++) {
+                    if (tmpDT[1] == Months[i]) {
+                      tmpMonth = i + 1;
+                    }
+                    if (tmpNow[1] == Months[i]) {
+                      nTmpMonth = i + 1;
+                    }
                   }
-                  if (tmpNow[1] == Months[i]) {
-                    nTmpMonth = i + 1;
-                  }
-                }
-                const month = tmpMonth;
-                const nMonth = nTmpMonth;
-                const Disable =
-                  nYear !== year ||
-                  nMonth !== month ||
-                  nDay !== day ||
-                  nHour !== Hour ||
-                  nMin !== Min;
+                  const month = tmpMonth;
+                  const nMonth = nTmpMonth;
+                  const Disable =
+                    nYear !== year ||
+                    nMonth !== month ||
+                    nDay !== day ||
+                    nHour !== Hour ||
+                    nMin !== Min;
 
-                return (
-                  <tr
-                    className={classnames("", {
-                      P_Alert:
-                        (i.valve === 1 && Number(i.flow) === 0) ||
-                        (i.valve === 0 && Number(i.flow) > 0),
-                    })}
-                    onClick={() => getBoardData(i.b_id, i.b_name)}
-                  >
-                    <td>{k + 1}</td>
-                    <td>{i.b_name}</td>
-                    <td>
-                      {Disable ? (
-                        <span className="offline">OFFLINE</span>
-                      ) : (
-                        <span className="online">ONLINE</span>
-                      )}
-                    </td>
-                    <td>{i.boardTemp}</td>
-                    <td>{i.boardHum}</td>
-                    <td>{i.ph}</td>
-                    <td>{i.phTemp}</td>
-                    <td>{i.ec}</td>
-                    <td>{i.ecTemp}</td>
-                    <td>{i.relay1Name}</td>
-                    <td>
-                      {i.scc === 0 ? (
-                        <span className="offline">OFF</span>
-                      ) : (
-                        <span className="online">ON</span>
-                      )}
-                    </td>
-                    <td>{i.relay2Name}</td>
-                    <td>
-                      {i.valve === 0 ? (
-                        <span className="offline">OFF</span>
-                      ) : (
-                        <span className="online">ON</span>
-                      )}
-                    </td>
-                    <td>{i.relay3Name}</td>
-                    <td>
-                      {i.bcl === 0 ? (
-                        <span className="offline">OFF</span>
-                      ) : (
-                        <span className="online">ON</span>
-                      )}
-                    </td>
-                    <td>
-                      {(i.valve === 1 && Number(i.flow) === 0) ||
-                      (i.valve === 0 && Number(i.flow) > 0) ? (
-                        <span className="offline">DANGER</span>
-                      ) : (
-                        <span className="online">SAVE</span>
-                      )}
-                    </td>
-                  </tr>
-                );
+                  return (
+                    <tr
+                      className={classnames("", {
+                        P_Alert:
+                          (i.valve === 1 && Number(i.flow) === 0) ||
+                          (i.valve === 0 && Number(i.flow) > 0),
+                      })}
+                      onClick={() => getBoardData(i.b_id, i.b_name)}
+                    >
+                      <td>{k + 1}</td>
+                      <td>{i.b_name}</td>
+                      <td>
+                        {Disable ? (
+                          <span className="offline">OFFLINE</span>
+                        ) : (
+                          <span className="online">ONLINE</span>
+                        )}
+                      </td>
+                      <td>{i.boardTemp}</td>
+                      <td>{i.boardHum}</td>
+                      <td>{i.ph}</td>
+                      <td>{i.phTemp}</td>
+                      <td>{i.ec}</td>
+                      <td>{i.ecTemp}</td>
+                      <td>{i.relay1Name}</td>
+                      <td>
+                        {i.scc === 0 ? (
+                          <span className="offline">OFF</span>
+                        ) : (
+                          <span className="online">ON</span>
+                        )}
+                      </td>
+                      <td>{i.relay2Name}</td>
+                      <td>
+                        {i.valve === 0 ? (
+                          <span className="offline">OFF</span>
+                        ) : (
+                          <span className="online">ON</span>
+                        )}
+                      </td>
+                      <td>{i.relay3Name}</td>
+                      <td>
+                        {i.bcl === 0 ? (
+                          <span className="offline">OFF</span>
+                        ) : (
+                          <span className="online">ON</span>
+                        )}
+                      </td>
+                      <td>
+                        {(i.valve === 1 && Number(i.flow) === 0) ||
+                        (i.valve === 0 && Number(i.flow) > 0) ? (
+                          <span className="offline">DANGER</span>
+                        ) : (
+                          <span className="online">SAVE</span>
+                        )}
+                      </td>
+                    </tr>
+                  );
+                }
               })}
             </tbody>
           </table>
